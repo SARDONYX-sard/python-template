1b24988cc222e9439d2de2cf9f8adeb471d17473
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-12-25 08:13:29 +0900

    style: lint fix

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -1,13 +1,13 @@
-from src import __version__
 import unittest
+
+from src import __version__
 from src.utils.operator import add
 
 
 class TestAdd(unittest.TestCase):
-
-    def test_evalate(self):
+    def test_evalate(self) -> None:
         self.assertEqual("2", f"{add(1, 1)}")
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_version() -> None:
+    assert __version__ == "0.1.0"
